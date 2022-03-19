1a66cf6f95b2880b933b1b6ade17f93a4369ab09
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-03-19 21:16:08 +0100

    initial commit

---
+++ apps/professional-ngrx/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'professional-ngrx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'professional-ngrx';
+}
